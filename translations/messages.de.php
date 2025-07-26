44465fbc59f49efa9982aabf5e06d469d4148521
Luca Schultz <github@lucaschultz.com>
2025-07-26 13:54:08 +0200

    chore: set up translations
    Signed-off-by: Luca Schultz <github@lucaschultz.com>

---
+++ translations/messages.de.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+  "welcome" => "Willkommen bei StudiBonnTask",
+  "lucky_number" => "Your lucky number is",
+  "task" => [
+    "status" => [
+      "todo" => "Zu erledigen",
+      "in_progress" => "In Bearbeitung",
+      "done" => "Erledigt",
+    ],
+    "priority" => [
+      "low" => "Niedrig",
+      "medium" => "Mittel",
+      "high" => "Hoch",
+    ],
+    "create" => [
+      "success" => "Aufgabe erfolgreich erstellt!",
+      "error" => "Fehler beim Erstellen der Aufgabe",
+    ],
+  ],
+  "user" => [
+    "login" => "Anmelden",
+    "logout" => "Abmelden",
+    "register" => "Registrieren",
+  ],
+];
