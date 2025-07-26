efa18f29967612ec570f1668dae9487fae450ad6
Luca Schultz <github@lucaschultz.com>
2025-07-26 14:04:21 +0200

    chore: make user model timestampable
    Signed-off-by: Luca Schultz <github@lucaschultz.com>

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -11,6 +12,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Table(name: "`user`")]
 #[ORM\UniqueConstraint(name: "UNIQ_IDENTIFIER_EMAIL", fields: ["email"])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface {
+  use TimestampableEntity;
+
   #[ORM\Id]
   #[ORM\GeneratedValue]
   #[ORM\Column]
