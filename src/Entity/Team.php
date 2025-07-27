fc103b51a049e0bea5fbf925623e9ab944725997
Luca Schultz <github@lucaschultz.com>
2025-07-27 08:17:12 +0200

    chore: add team entity and migrations
    Signed-off-by: Luca Schultz <github@lucaschultz.com>

---
+++ src/Entity/Team.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeamRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+#[ORM\Entity(repositoryClass: TeamRepository::class)]
+class Team {
+  use TimestampableEntity;
+
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
+
+  #[ORM\Column(length: 100)]
+  private ?string $name = null;
+
+  #[ORM\Column(type: Types::TEXT, nullable: true)]
+  private ?string $description = null;
+
+  public function getId(): ?int {
+    return $this->id;
+  }
+
+  public function getName(): ?string {
+    return $this->name;
+  }
+
+  public function setName(string $name): static {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  public function getDescription(): ?string {
+    return $this->description;
+  }
+
+  public function setDescription(?string $description): static {
+    $this->description = $description;
+
+    return $this;
+  }
+}
