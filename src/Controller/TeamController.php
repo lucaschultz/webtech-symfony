0182ed2908b6aecdeb38b5a7e4e133767ca33a46
Ahmad Assaf <ahmad-assaf@outlook.de>
2025-07-29 18:27:16 +0200

    feat: add access restrictions to teams actions in team show page

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -11,20 +11,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 final class TeamController extends AbstractController {
   #[Route("/teams/{id}", name: "app_team_show", requirements: ["id" => "\d+"])]
   public function show(Team $team): Response {
-    $this->denyAccessUnlessGranted(TeamVoter::VIEW, $team);
+    try {
+      $this->denyAccessUnlessGranted(TeamVoter::VIEW, $team);
+    } catch (AccessDeniedException $e) {
+      $this->addFlash("error", "You do not have permission to view this team.");
+      return $this->redirectToRoute("app_teams_list");
+    }
 
     return $this->render("team/show.html.twig", [
       "team" => $team,
+      "TEAM_EDIT" => TeamVoter::EDIT,
+      "TEAM_DELETE" => TeamVoter::DELETE,
     ]);
   }
 
   #[Route("/teams/new", name: "app_team_new")]
   public function new(Request $request, EntityManagerInterface $em): Response {
-    $this->denyAccessUnlessGranted("IS_AUTHENTICATED_FULLY");
+    try {
+      $this->denyAccessUnlessGranted("IS_AUTHENTICATED_FULLY");
+    } catch (AccessDeniedException $e) {
+      $this->addFlash("error", "Please login or create a new account first.");
+      return $this->redirectToRoute("app_home");
+    }
 
     $team = new Team();
 
@@ -51,13 +64,18 @@ final class TeamController extends AbstractController {
     ]);
   }
 
-  #[Route("/team/{id}/edit", name: "app_team_edit")]
+  #[Route("/teams/{id}/edit", name: "app_team_edit")]
   public function edit(
     Request $request,
     Team $team,
     EntityManagerInterface $em
   ): Response {
-    $this->denyAccessUnlessGranted(TeamVoter::EDIT, $team);
+    try {
+      $this->denyAccessUnlessGranted(TeamVoter::EDIT, $team);
+    } catch (AccessDeniedException $e) {
+      $this->addFlash("error", "You do not have permission to edit this team.");
+      return $this->redirectToRoute("app_team_show", ["id" => $team->getId()]);
+    }
 
     $form = $this->createForm(TeamNewType::class, $team);
 
@@ -92,7 +110,15 @@ final class TeamController extends AbstractController {
     Team $team,
     EntityManagerInterface $em
   ): Response {
-    $this->denyAccessUnlessGranted(TeamVoter::DELETE, $team);
+    try {
+      $this->denyAccessUnlessGranted(TeamVoter::DELETE, $team);
+    } catch (AccessDeniedException $e) {
+      $this->addFlash(
+        "error",
+        "You do not have permission to delete this team."
+      );
+      return $this->redirectToRoute("app_team_show", ["id" => $team->getId()]);
+    }
 
     $em->remove($team);
     $em->flush();
