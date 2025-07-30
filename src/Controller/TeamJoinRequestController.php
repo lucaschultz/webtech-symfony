aeef4656337c3944d660cce46e1562616e7aa8bf
Ahmad Assaf <ahmad-assaf@outlook.de>
2025-07-30 17:00:59 +0200

    feat: add new notifications related to team joining requests for team owner and requester

--- src/Controller/TeamJoinRequestController.php
+++ src/Controller/TeamJoinRequestController.php
@@ -4,18 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Team;
 use App\Entity\TeamJoinRequest;
+use App\Service\AppNotificationService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TeamJoinRequestController extends AbstractController {
-  //   #[Route("/team/join-request", name: "app_team_join_request")]
-  //   public function index(): Response {
-  //     return $this->render("team_join_request/index.html.twig", [
-  //       "controller_name" => "TeamJoinRequestController",
-  //     ]);
-  //   }
+  private AppNotificationService $notificationService;
+
+  public function __construct(AppNotificationService $notificationService) {
+    $this->notificationService = $notificationService;
+  }
 
   #[
     Route(
@@ -28,6 +28,7 @@ final class TeamJoinRequestController extends AbstractController {
     Team $team,
     EntityManagerInterface $em
   ): Response {
+    /** @var \App\Entity\User $user */
     $user = $this->getUser();
 
     // Prevent duplicate requests
@@ -58,6 +59,15 @@ final class TeamJoinRequestController extends AbstractController {
     $em->persist($joinRequest);
     $em->flush();
 
+    $this->notificationService->notifyUsers(
+      [$team->getCreatedBy()],
+      'New join request for your team "' .
+        $team->getName() .
+        '" by ' .
+        $user->getFullName(),
+      $this->generateUrl("app_team_show", ["id" => $team->getId()])
+    );
+
     $this->addFlash(
       "success",
       "Request sent! The team owner will review your request."
@@ -77,6 +87,7 @@ final class TeamJoinRequestController extends AbstractController {
     TeamJoinRequest $joinRequest,
     EntityManagerInterface $em
   ): Response {
+    /** @var \App\Entity\User $user */
     $user = $this->getUser();
     $team = $joinRequest->getTeam();
 
@@ -89,6 +100,14 @@ final class TeamJoinRequestController extends AbstractController {
     $joinRequest->setStatus(TeamJoinRequest::STATUS_ACCEPTED);
     $team->addUser($joinRequest->getRequester());
     $em->flush();
+
+    $this->notificationService->notifyUsers(
+      [$joinRequest->getRequester()],
+      'Your request to join team "' .
+        $team->getName() .
+        '" was approved! Click to view the team.',
+      $this->generateUrl("app_team_show", ["id" => $team->getId()])
+    );
 
     $this->addFlash("success", "Request accepted and member added!");
     return $this->redirectToRoute("app_team_show", ["id" => $team->getId()]);
@@ -115,6 +134,12 @@ final class TeamJoinRequestController extends AbstractController {
 
     $joinRequest->setStatus(TeamJoinRequest::STATUS_DECLINED);
     $em->flush();
+
+    $this->notificationService->notifyUsers(
+      [$joinRequest->getRequester()],
+      'Your request to join team "' . $team->getName() . '" was declined.',
+      $this->generateUrl("app_teams_list")
+    );
 
     $this->addFlash("info", "Request declined.");
     return $this->redirectToRoute("app_team_show", ["id" => $team->getId()]);
